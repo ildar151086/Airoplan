ba1df337d5f664a951569e78a99bc581c88ad8d5
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-05-05 15:25:18 +0500

    2

--- Airplane.cpp
+++ Airplane.cpp
@@ -1,8 +1,9 @@
 #include "Airplane.h"
-#include<string>
+#include <string>
 #include <iostream>
 
 using namespace std;
+
 
 Airplane::Airplane() {
 	countUser = 0;
@@ -11,16 +12,18 @@ Airplane::Airplane() {
 	weightMaximum = 0;
 	flightAltitudeMax = 0;
 	name = "";
+	passengerBoard(0);
 }
 
 Airplane::Airplane(int countUser, int velKreyser, int rangeFlight,
-				   int weightMaximum, int flightAltitudeMax, string name) {
+	int weightMaximum, int flightAltitudeMax, string name) {
 	this->countUser = countUser;
 	this->velKreyser = velKreyser;
 	this->rangeFlight = rangeFlight;
 	this->weightMaximum = weightMaximum;
 	this->flightAltitudeMax = flightAltitudeMax;
 	this->name = name;
+	passengerBoard(0);
 }
 
 int Airplane::getCountUser() {
@@ -52,18 +55,21 @@ void Airplane::print() {
 	cout << "Макисмлаьная высота полета: " << flightAltitudeMax << endl;
 }
 
-bool Airplane::operator==(const Airplane &aira) {
+
+bool Airplane::operator== (const Airplane& aira) {
 	if (name == aira.name) {
 		return true;
-	} else {
+	}
+	else {
 		return false;
 	}
 }
 
-bool Airplane::operator> (const Airplane &aira) {
+bool Airplane::operator> (const Airplane& aira) {
 	if (countUser > aira.countUser) {
 		return true;
-	} else {
+	}
+	else {
 		return false;
 	}
 }
@@ -77,6 +83,37 @@ bool Airplane::operator< (const Airplane& aira) {
 	}
 }
 
-void Airplane::operator++ () {
-	this->countUser += 10;
+Airplane Airplane::operator& (Airplane const& second) {
+	this->countUser = second.countUser;
+	this->flightAltitudeMax = second.flightAltitudeMax;
+	this->name = second.name;
+	this->rangeFlight = second.rangeFlight;
+	this->velKreyser = second.velKreyser;
+	this->weightMaximum = second.weightMaximum;
+	return *this;
+}
+
+
+/// <summary>
+/// Создаем массив пасажиров и заполняем его
+/// </summary>
+/// <param name="count">Количество пассажиров</param>
+void Airplane::passengerBoard(int count) {	
+	if (count != 0) {
+		// Проверяем чтобы количество пассажиров было не больше чем помещаеться
+		passengerCount = (count <= countUser) ? count : countUser;
+		parrayPass = new Passenger[passengerCount];
+		for (int i = 0; i < passengerCount; i++) {
+			parrayPass[i].setName("Name ");
+			parrayPass[i].setAge("age ");
+			parrayPass[i].setWieght("wiegth ");
+			parrayPass[i].setBaggage(1);
+		}
+	}
+}
+
+void Airplane::printPassenger() {
+	for (int i = 0; i < passengerCount; i++) {
+		parrayPass[i].print();
+	}
 }
--- Airplane.h
+++ Airplane.h
@@ -10,10 +10,64 @@
 Для класса самолет реализовать массив пассажиров
 */
 #include<string>
+#include <iostream>
 
 using namespace std;
 
-class Airplane{
+int const STEP_COUNT = 10;
+
+class Passenger {
+	string name;
+	string age;
+	string wieght;
+	bool baggage;// есть багаж или нет
+public:
+	Passenger() {
+		name = "";
+		age = "";
+		wieght = "";
+		baggage = false;
+	}
+	Passenger(string name, string age, string wieght, bool baggage) {
+		this->name = name;
+		this->age = age;
+		this->wieght = wieght;
+		this->baggage = false;
+	}
+	string getName() {
+		return name;
+	}
+	string getAge() {
+		return age;
+	}
+	string getWieght() {
+		return wieght;
+	}
+	bool getBaggage() {
+		return baggage;
+	}
+	void setName(string value) {
+		name = value;
+	}
+	void setAge(string value) {
+		age = value;
+	}
+	void setWieght(string value) {
+		wieght = value;
+	}
+	void setBaggage(bool value) {
+		baggage = value;
+	}
+	void print() {
+		cout << getName() << endl;
+		cout << getAge() << endl;
+		cout << getWieght() << endl;
+		cout << getBaggage() << endl;
+	}
+
+};
+
+class Airplane {
 	/// Число мест
 	int countUser;
 	/// Крейсерская скорость км/с
@@ -26,21 +80,45 @@ class Airplane{
 	int flightAltitudeMax;
 	/// Модель самолета
 	string name;
+	/// Массив для хранения пассажиров 
+	Passenger* parrayPass;
+	/// Количество пассажиров
+	int passengerCount;
 public:
 	Airplane();
-	Airplane(int, int , int , int , int , string);
+	Airplane(int, int, int, int, int, string);
 
-	void print();
+
 	int getCountUser();
 	int getvelKreyser();
 	int getrangeFlight();
 	int getweightMaximum();
 	int getflightAltitudeMax();
 	string getname();
+	
+	void print();
+	void passengerBoard(int count);
+	void printPassenger();
+	
+	Airplane operator& (Airplane const& second);
+	bool operator==(const Airplane& aira);
+	bool operator> (const Airplane& aira);
+	bool operator< (const Airplane& aira);
+	Airplane& operator++ () {
+		this->countUser += STEP_COUNT;
+		return *this;
+	}
+	Airplane& operator-- () {
+		this->countUser -= STEP_COUNT;
+		return *this;
+	}
 
-	bool operator== (const Airplane&);
-	bool operator> (const Airplane&);
-	bool operator< (const Airplane&);
-	void operator++ ();
+	~Airplane() {
+		if (passengerCount != 0) {
+			delete[] parrayPass;
+		}
+	}
 };
+
+
 
